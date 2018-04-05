d4f372ad772a13bf30b1dbfaefa3ed6f5de4d79c
Marissa Twitchell <twitchell.marissa@gmail.com>
2018-04-05 09:04:30 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+/*Tracking*/
+var ua = require('universal-analytics');
+var visitor = ua('UA-82624283-1');
+visitor.pageview("/").send();
